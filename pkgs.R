58b4818bd663a97dc9e5f52e7680051581ba12c4
Abhijit Dasgupta <aikidasgupta@gmail.com>
2019-03-19 23:10:57 -0400

    package download code

---
+++ pkgs.R
@@ -0,0 +1,44 @@
+library(XML)
+library(RCurl)
+library(tidyverse)
+
+theurl <- "http://cran.rstudio.com/bin/windows/contrib/3.5/"
+webpage = getURL(theurl)
+webpage <- readLines(tc <- textConnection(webpage)); close(tc)
+
+zipfiles <- webpage[str_detect(webpage, 'zip')]
+zips <- str_extract(zipfiles, '[A-Za-z0-9\\.]+_[0-9\\.\\-]+\\.zip')
+zip_info <- do.call(rbind,
+                    str_match_all(zips, '(^[a-zA-Z0-9\\.\\-]+)_([0-9\\.\\-]+)\\.zip'))
+zip_info <- as.data.frame(zip_info) %>% set_names(c('zipfile','basename','version'))
+basenames <- str_extract(zips, '^[A-Za-z0-9\\.]+')
+
+installed_pkgs <- read_csv('Documentation_Private/package.csv')
+
+pkg_wish <- read_csv('Documentation_Private/pkg_wish.txt', col_names = FALSE)
+deps <- tools::package_dependencies(pkg_wish$X1, recursive = T)
+top_pkgs <- names(deps)
+deps <- unlist(deps)
+deps <- c(deps, top_pkgs)
+deps <- unname(deps)
+deps <- unique(deps)
+
+pkgs_for_install <- setdiff(deps, installed_pkgs$Package)
+
+
+#
+# pkgs <- unlist(read_yaml('Documentation_Private/packages.yml'))
+# n <- names(pkgs) %>% str_remove('\\d+') %>% unique() %>% sort()
+# needed_pkgs <- sort(unique(c(pkgs, n)))[-1]
+#
+#
+# pkgs_for_install <- setdiff(needed_pkgs, installed_pkgs$Package)
+
+zips_to_download <- zips[basenames %in% pkgs_for_install]
+setwd('pkgs_for_download/')
+for (z in zips_to_download){
+  print(z)
+  URL <- paste0(theurl, z)
+  f <- getBinaryURL(URL, verbose = T)
+  writeBin(f, fs::path('pkgs_for_download',z))
+}
